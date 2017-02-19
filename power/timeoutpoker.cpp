Unlegacy-Android/android_device_asus_grouper

power hal: requestPmQosTimed: remove logcat spam
Change-Id: I94ba1039bc58f7584a14de6a22fc9a9e8e548055

power/timeoutpoker.cpp
@@ -112,7 +112,7 @@ void TimeoutPoker::requestPmQosTimed(const char* filename,
 {
     pushEvent(new PmQosOpenTimedEvent(
                 filename, val, timeout));
-    ALOGI("starting requestPmQosTimed, mNodeName %s, mQosValue %d, timeout %d milliseconds",
+    ALOGD("starting requestPmQosTimed, mNodeName %s, mQosValue %d, timeout %d milliseconds",
             filename, val, int(ns2ms(timeout)));
 }
 

Unlegacy-Android/android_hardware_nvidia_tegra3

power/timeoutpoker.cpp
@@ -112,7 +112,7 @@ void TimeoutPoker::requestPmQosTimed(const char* filename,
 {
     pushEvent(new PmQosOpenTimedEvent(
                 filename, val, timeout));
-    ALOGI("starting requestPmQosTimed, mNodeName %s, mQosValue %d, timeout %d milliseconds",
+    ALOGV("starting requestPmQosTimed, mNodeName %s, mQosValue %d, timeout %d milliseconds",
             filename, val, int(ns2ms(timeout)));
 }
 