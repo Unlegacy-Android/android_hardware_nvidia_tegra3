7a0cd7419486730cd2d61e783f75e9ee131ca214
Ziyan <jaraidaniel@gmail.com>
2017-04-09 12:07:14 +0200

    sepolicy: fix 6.0/7.1 compatibility
    Change-Id: I2ece6f1ae114e042b4e34bd014604b2821836d9a

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,28 @@
+TEGRA3_SEPOLICY_BASE := hardware/nvidia/tegra3/sepolicy
+
+# 4.4
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 19 || echo 1),)
+BOARD_SEPOLICY_UNION += \
+	app.te \
+	device.te \
+	drmserver.te \
+	file.te \
+	file_contexts \
+	init.te \
+	surfaceflinger.te \
+	system_app.te \
+	ueventd.te
+endif
+
+# 6.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 23 || echo 1),)
+BOARD_SEPOLICY_DIRS += $(TEGRA3_SEPOLICY_BASE)/compat/mm
+endif
+
+# >=7.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 24 || echo 1),)
+BOARD_SEPOLICY_M4DEFS += android_api_mm_or_later=true
+BOARD_SEPOLICY_M4DEFS += android_api_n_or_later=true
+endif
+
+BOARD_SEPOLICY_DIRS += $(TEGRA3_SEPOLICY_BASE)
